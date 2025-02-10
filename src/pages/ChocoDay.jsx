3ee474936ee7b356284951acea5128d0a8e37088
Shubh-WebD <sharma.shubham2124@gmail.com>
2025-02-10 22:32:46 +0530

    modifications

--- src/pages/ChocoDay.jsx
+++ src/pages/ChocoDay.jsx
@@ -6,13 +6,13 @@ const ChocoDay = () => {
         <div className="w-full min-h-screen flex flex-col justify-center items-center">
             <FloatingEle />
             <div className="flex-grow flex w-full justify-evenly items-center">
-            <div className='flex flex-col'>
-                <p className='text-[22px] mb-6' style={{ fontFamily: "Pacifico, cursive" }}>Happy Chocolate day !!</p>
-                <p>You tease, you nag, you drive me mad</p>
-                <p>Yet without you, I&apos;d be so sad!</p>
-                <p>But one thing is true, come what my way</p>
-                <p>I&apos;d pick you over chocolate any day!</p>
-            </div>
+                <div className='flex flex-col'>
+                    <p className='text-[22px] mb-6' style={{ fontFamily: "Pacifico, cursive" }}>Happy Chocolate day !!</p>
+                    <p style={{ fontFamily: "Courgette, cursive" }}>You tease, you nag, you drive me mad</p>
+                    <p style={{ fontFamily: "Courgette, cursive" }}>Yet without you, I&apos;d be so sad!</p>
+                    <p style={{ fontFamily: "Courgette, cursive" }}>But one thing is true, come what my way</p>
+                    <p style={{ fontFamily: "Courgette, cursive" }}>I&apos;d pick you over chocolate any day!</p>
+                </div>
             </div>
             <Footer />
         </div>
--- src/pages/PromiseDay.jsx
+++ src/pages/PromiseDay.jsx
@@ -6,32 +6,34 @@ const PromiseDay = () => {
             <div className="flex-grow flex flex-col w-full justify-evenly items-center sm:flex p-4 sm:p-0">
                 <div className="flex flex-col mt-6">
                     <p className='text-[22px]' style={{ fontFamily: "Pacifico, cursive" }}>Happy Promise day !!</p>
-                    <audio className="mt-10 mb-4" controls>
+                    <p className="mt-10 mb-4" style={{ fontFamily: "Courgette, cursive" }}>Main shayar to nhi...pr ae haathi <br></br>
+                        jb se tumhe dekha mujhe average si poem likni aa gyi</p>
+                    <audio className="mt-4 mb-4" controls>
                         <source src={promise} type="audio/mp4" />
                     </audio>
-                    <p className="mt-4 mb-4">Ye to maine bta dia hai bohot baar <br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mt-4 mb-4">Ye to maine bta dia hai bohot baar <br></br>
                         Ki hai mujhe tumse kitna pyaar</p>
-                    <p className="mb-4">To chlo ab krte hai kuch kaam ki baat <br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">To chlo ab krte hai kuch kaam ki baat <br></br>
                         Jeeni jo hai poori life tumhare saath</p>
-                    <p className="mb-4">Sun lo mere tumse jo hai ye waadein <br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Sun lo mere tumse jo hai ye waadein <br></br>
                         Mujhe tumhare saath banani hai bohot yaadein</p>
-                    <p className="mb-4">I know at times ho jati hu thodi zda rude<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">I know at times ho jati hu thodi zda rude<br></br>
                         But, I promise ki humesha theek kr dugi tumhara mood</p>
-                    <p className="mb-4">Wait krugi tumhara humesha...chahe ho jao kitne bhi late<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Wait krugi tumhara humesha...chahe ho jao kitne bhi late<br></br>
                         Zda daaru peekr aaye to nhi kholugi ghr ka gate</p>
-                    <p className="mb-4">Bina kisi demand ke krugi tumhari sewa<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Bina kisi demand ke krugi tumhari sewa<br></br>
                         Bss tum mujhe koi insecurities ka reason mt dena</p>
-                    <p className="mb-4">Mummy, papa, shanno...sbko krugi bohot pyar<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Mummy, papa, shanno...sbko krugi bohot pyar<br></br>
                         Tum bss kbhi kbhi chai aur maggie khila dena yaar</p>
-                    <p className="mb-4">Kudh se phele... krugi poori tumhari saari dreams<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Kudh se phele... krugi poori tumhari saari dreams<br></br>
                         Tum time pe meri insta pe bheji hui dekhna sb reels</p>
-                    <p className="mb-4">Hai tumhare lye ye situation win-win<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Hai tumhare lye ye situation win-win<br></br>
                         Kabhi kr lia kro mere saath twining</p>
-                    <p className="mb-4">Chd ke kabhi na tumhe jaugi<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Chd ke kabhi na tumhe jaugi<br></br>
                         Tumhari hrr khushi...hrr dukh apna smjh ke nibhaugi</p>
-                    <p className="mb-4">Ab lines aur mil nhi rhi hai kuch khass<br></br>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className="mb-4">Ab lines aur mil nhi rhi hai kuch khass<br></br>
                         Btao? Can you spend your whole life listening to my bkwaas?</p>
-                    <p className='mt-10 mb-4'>Maine he likhi hai ye....</p>
+                    <p style={{ fontFamily: "Courgette, cursive" }} className='mt-10 mb-4'>Maine he likhi hai ye....</p>
                 </div>
             </div>
             <Footer />
